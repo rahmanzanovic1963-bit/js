7d4bc33a32f1b61737a50bbb4b771e885476ecaa
kutmanbek359-dev <kutmanbek359@gmail.com>
2026-09-22 16:02:14 +0600

    lesson3

---
+++ lesson3/lesson.js
@@ -0,0 +1,93 @@
+// let email = prompt ("ведите емайл")
+// let password = prompt ("ведите пароль")
+// let useremail =  'admin'
+// let userpassword = 'admin123'
+
+// if( email === useremail && password === userpassword ) {
+//     alert("вывошли ак")
+// }else{
+//     alert("вы не вошли ак")
+// }
+
+// let egeg = prompt("сан жаз");
+// if (egeg < 0) {
+//   alert("не отрецатель");
+// } else if (ageg > 0) {
+//   alert("отрецатель");
+// } else {
+//   alert("число равен нулю");
+// }
+
+// let month = prompt("месяц");
+// switch (month) {
+//   case "январь":
+//   case "февраль":
+//   case "декабрь":
+//     alert("зима");
+//       break;
+//       case "март":
+//      case "апрель":
+//      case "май":
+//      alert("весна");
+//       break;
+//       case "июнь":
+//      case "июль":
+//      case "август":
+//      alert("лето");
+//       break;
+//       case "сентябрь":
+//      case "октябрь":
+//      case "ноябрь":
+//      alert("осень");
+
+
+// }
+
+let month = prompt("месяц");
+switch (month) {
+  case "январь":
+  case "февраль":
+  case "декабрь":
+    alert("ош");
+      break;
+      case "март":
+     case "апрель":
+     case "май":
+     alert("баткен");
+      break;
+      case "июнь":
+     case "июль":
+     case "август":
+     alert("нарын");
+      break;
+      case "сентябрь":
+     case "октябрь":
+     case "ноябрь":
+     alert("чуй");
+      break;
+      case "сентябрь":
+     case "октябрь":
+     case "ноябрь":
+     alert("манас");
+      break;
+      case "сентябрь":
+     case "октябрь":
+     case "ноябрь":
+     alert("ысык кол");
+      break;
+      case "сентябрь":
+     case "октябрь":
+     case "ноябрь":
+     alert("талас");
+
+
+}
+
+
+
+
+
+
+
+
+
